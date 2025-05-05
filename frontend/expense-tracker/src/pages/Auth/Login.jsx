963ae256e9abd280d9c843d06890693e3650a78f
Angelo <180010005@usc.edu.ph>
2025-05-05 23:42:37 +0800

    20/03/2025 - create layout for Authentication Page

--- frontend/expense-tracker/src/pages/Auth/Login.jsx
+++ frontend/expense-tracker/src/pages/Auth/Login.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
+import AuthLayout from '../../components/layouts/AuthLayout'
+import { useNavigate } from 'react-router-dom';
+import Input from "../../components/Inputs/Input";
+import { Link } from 'react-router-dom';
+import { validateEmail } from '../../utils/helper'; 
+import { API_PATHS, BASE_URL } from '../../utils/apiPaths';
+import axiosInstance from '../../utils/axiosInstance';
+import { UserContext } from '../../context/UserContext';
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
+
 }
 
-export default Login
+export default Login;
