e45557668b3c2fe14ce12e1f20c6a1dc55b9cdf1
maojude <maojude04@gmail.com>
2025-05-06 21:08:05 +0800

    25/03/2025 - Create Income route

---
+++ backend/routes/incomeRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const {
+    addIncome,
+    getAllIncome,
+    deleteIncome,
+    
+} = require("../controllers/incomeController");
+
+const { protect } = require('../middleware/authMiddleware');
+
+const router = express.Router();
+
+router.post("/add", protect, addIncome);
+router.get("/get", protect, getAllIncome);
+router.delete("/:id", protect, deleteIncome);
+
+module.exports = router;
