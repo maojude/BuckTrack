b86c220727de800eb34fda267123a683fbfb7fce
maojude <maojude04@gmail.com>
2025-05-06 18:27:30 +0800

    21/03/2025 - Configure routes for authentication to be used in authController

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const{
+    registerUser,
+    loginUser,
+    getUserInfo,
+    googleLogin,
+
+} = require('../controllers/authController.js');
+
+const router = express.Router();
+
+router.post('/register', registerUser);
+
+router.post('/login', loginUser);
+
+router.get('/getUser', protect, getUserInfo);
+
+
+module.exports = router;
+// This code defines the routes for user authentication in an Express application. 
+// It imports the necessary modules, defines the routes for user registration, 
+// login, and fetching user information, and exports the router for use in other 
+// parts of the application.
+
