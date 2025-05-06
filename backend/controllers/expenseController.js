19bab816dcae76eb9e3d62cdae6f64bfd408423e
maojude <maojude04@gmail.com>
2025-05-06 21:15:38 +0800

    25/03/2025 - Create expense controller

--- backend/controllers/expenseController.js
+++ backend/controllers/expenseController.js
@@ -1,16 +1,52 @@
-const express = require('express');
-const {
-    addExpense,
-    getAllExpense,
-    deleteExpense,
-} = require("../controllers/expenseController");
+const User = require('../models/User');
+const Expense = require('../models/Expense'); //load Expense model to interact with database   
 
-const { protect } = require('../middleware/authMiddleware');
+// Add Expense Source
+exports.addExpense = async (req, res) => {
+    const userId = req.user.id;
 
-const router = express.Router();
+    try{
+        const{ icon, category, amount, date } = req.body; 
 
-router.post("/add", protect, addExpense);
-router.get("/get", protect, getAllExpense);
-router.delete("/:id", protect, deleteExpense);
+        // Validation: Check for missing fields
+        if(!category || !amount || !date){
+            return res.status(400).json({ message: "All fields are required" });
+        }
 
-module.exports = router;
+        const newExpense = new Expense({
+            userId,
+            icon,
+            category,
+            amount,
+            date: new Date(date)
+        });
+        
+        await newExpense.save(); // Save the new expense to the database
+        res.status(200).json(newExpense);
+    }catch(error){
+        res.status(500).json({ message: "Server error" });
+    }
+}
+
+// Get ALL Expense Source
+exports.getAllExpense = async (req, res) => {
+    const userId = req.user.id;
+
+    try{
+        const expense = await Expense.find({ userId }).sort({ date: -1 }); // Sort by date in descending order
+        res.json(expense); // Return the expense data as JSON
+    }catch(error){
+        res.status(500).json({ message: "Server error" });
+    }
+}
+
+// Delete Expense Source
+exports.deleteExpense = async (req, res) => {
+    try{
+        await Expense.findByIdAndDelete(req.params.id); // Find and delete the expense by ID and userId
+        res.json({ message: "Expense deleted successfully" }); // Respond with success message
+    }catch(error){
+        res.status(500).json({ message: "Server error" });
+    }
+}
+
