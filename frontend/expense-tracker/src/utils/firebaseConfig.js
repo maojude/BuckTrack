8d31ac5b61c01e0560d0b35f40b86bf034364b66
maojude <maojude04@gmail.com>
2025-05-07 23:00:14 +0800

    15/04/2025 - Changed and modified firebase configurations

--- frontend/expense-tracker/src/utils/firebaseConfig.js
+++ frontend/expense-tracker/src/utils/firebaseConfig.js
@@ -1,17 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBO5rRtqY1Z2kwYBYimKEseNomXxWDzMTs",
-  authDomain: "bucktrack-v1.firebaseapp.com",
-  projectId: "bucktrack-v1",
-  storageBucket: "bucktrack-v1.firebasestorage.app",
-  messagingSenderId: "306910393577",
-  appId: "1:306910393577:web:9ff0d98530ef22d1d52c78"
+    apiKey: "AIzaSyB8vYGXU-yT2MT3lBE6Bbg7r0O1KUCHSdA",
+    authDomain: "bucktrack-final.firebaseapp.com",
+    projectId: "bucktrack-final",
+    storageBucket: "bucktrack-final.firebasestorage.app",
+    messagingSenderId: "191175339154",
+    appId: "1:191175339154:web:e4f1f636bf7145d6034a4b"
 };
 
 // Initialize Firebase
