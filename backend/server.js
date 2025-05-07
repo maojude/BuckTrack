47c36341c36c72c2e39fde3db469474d3959b2a3
maojude <maojude04@gmail.com>
2025-05-07 23:01:18 +0800

    15/04/2025 - add firebase admin to server

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ const authRoutes = require("./routes/authRoutes");
 const incomeRoutes = require("./routes/incomeRoutes");
 const expenseRoutes = require("./routes/expenseRoutes");
 const dashboardRoutes = require("./routes/dashboardRoutes");
+const adminFirebase = require("./firebaseAdmin");
 
 const app = express();
 
