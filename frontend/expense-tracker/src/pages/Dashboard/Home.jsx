da639bd58befd7352305ec2b3635508e6236f768
maojude <maojude04@gmail.com>
2025-05-09 22:42:50 +0800

    18/04/2025 - Add fetchDashboardData function that fetches data

--- frontend/expense-tracker/src/pages/Dashboard/Home.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Home.jsx
@@ -1,12 +1,56 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import DashboardLayout from '../../components/layouts/DashboardLayout';
+import { useUserAuth } from '../../hooks/useUserAuth';
+import axiosInstance from '../../utils/axiosInstance';
+import { API_PATHS } from '../../utils/apiPaths';
+import { useNavigate } from 'react-router-dom';
 
 const Home = () => {
   useUserAuth();
+
+  const navigate = useNavigate();
+
+  const [ dashboardData, setDashboardData ] = useState(null);
+  const [ loading, setLoading ] = useState(false);
+  
+  const fetchDashboardData = async () => {
+    if (loading) return;
+
+    setLoading(true);
+
+    try{
+      const response = await axiosInstance.get(
+        `${API_PATHS.DASHBOARD.GET_DATA}`
+      );
+
+      if (response.data) {
+        setDashboardData(response.data);
+      }
+    } catch (error) {
+      console.log("Something went wrong. Please try again.", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchDashboardData();
+  
+    return () => {}
+  }, []);
+  
+
   return (
     <DashboardLayout activeMenu="Dashboard">
       <div className="my-5 mx-auto text-black">
-        Homes
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+          <InfoCard
+            icon={<IoMdCard />}
+            label="Total Balance"
+            value={addThousandSeparator(dashboardData?.totalBalance || 0)}
+            color="bg-primary"
+          />
+        </div>
       </div>
     </DashboardLayout>
   );
