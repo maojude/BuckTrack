7db41b5739a8b5563656c61360a9551ca0bc948d
JellyGels <shinken1008@gmail.com>
2025-05-10 18:51:02 +0800

    22/04/2025 - Add section for chart of Recent Income

--- frontend/expense-tracker/src/pages/Dashboard/Home.jsx
+++ frontend/expense-tracker/src/pages/Dashboard/Home.jsx
@@ -10,6 +10,7 @@ import RecentTransactions from '../../components/Dashboard/RecentTransactions';
 import FinanceOverview from '../../components/Dashboard/FinanceOverview';
 import ExpenseTransactions from '../../components/Dashboard/ExpenseTransactions';
 import Last30DaysExpenses from '../../components/Dashboard/Last30DaysExpenses';
+import RecentIncomeWithChart from '../../components/Dashboard/RecentIncomeWithChart';
 
 import { LuHandCoins, LuWalletMinimal } from 'react-icons/lu';
 import { IoMdCard } from 'react-icons/io';
@@ -96,6 +97,13 @@ const Home = () => {
             data={dashboardData?.last30DaysExpenses?.transactions || []}
           />
 
+          <RecentIncomeWithChart
+            data={dashboardData?.last30DaysIncome?.transactions?.slice(0,4) || []}
+            totalIncome={dashboardData?.totalIncome || 0}
+          />
+
+          
+
         </div>
       </div>
     </DashboardLayout>
@@ -104,4 +112,4 @@ const Home = () => {
 
 export default Home;
 
-// 2:51
+// 3:00:00
