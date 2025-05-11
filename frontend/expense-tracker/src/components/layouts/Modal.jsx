3a7969b70a8542baf29e688ed227a4429a92329d
JellyGels <shinken1008@gmail.com>
2025-05-11 18:29:19 +0800

    23/04/2025 - Create css styles to Modal

--- frontend/expense-tracker/src/components/layouts/Modal.jsx
+++ frontend/expense-tracker/src/components/layouts/Modal.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 
-const Modal = ({children, isOpen, onClose, title}) => {
+const Modal = ({children, isOpen, onClose, title}) => { 
+    
+    if(!isOpen) return null;
+    
   return <div className="fixed top-0 right-0 left-0 z-50 flex justify-center items-center w-full h-[calc(100%-1rem)] max-h-full overflow-y-auto overflox-x-hidden bg-black/20 bg-opacity-50">
     <div className="relative p-4 w-full max-w-2xl max-h-full">
         {/* Modal content */}
@@ -14,7 +17,7 @@ const Modal = ({children, isOpen, onClose, title}) => {
 
                 <button
                     type="button"
-                    className=""
+                    className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white cursor-pointer"
                     onClick={onClose}
                 >
                     X
@@ -24,7 +27,7 @@ const Modal = ({children, isOpen, onClose, title}) => {
             </div>
 
             {/* Modal body */}
-            <div className="">
+            <div className="p-4 md:p-5 space-y-4 ">
                 {children}
             </div>
         </div>
