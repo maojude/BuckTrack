7c31b900c7dfce662cf9b1a673e9fdcc650eef78
maojude <maojude04@gmail.com>
2025-05-12 21:38:15 +0800

    27/04/2025 - Usage of onEdit function

--- frontend/expense-tracker/src/components/Expense/ExpenseList.jsx
+++ frontend/expense-tracker/src/components/Expense/ExpenseList.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import moment from "moment";
 import TransactionInfoCard from "../Cards/TransactionInfoCard";
 
-const ExpenseList = ({ transactions, onDelete }) => {
+const ExpenseList = ({ transactions, onDelete, onEdit }) => {
   return (
     <div className="card">
       <div className="flex items-center justify-between">
         <h5 className="text-lg">Expense List</h5>
       </div>
 
-      <div className="grid grid-cols-1 mid:grid-cols-2">
+      <div className="grid grid-cols-1 md:grid-cols-2">
         {transactions?.map((expense) => (
           <TransactionInfoCard
             key={expense._id}
@@ -19,6 +19,7 @@ const ExpenseList = ({ transactions, onDelete }) => {
             amount={expense.amount}
             type="expense"
             onDelete={() => onDelete(expense._id)}
+            onEdit={() => onEdit(expense._id)} // Pass the expense ID to the onEdit function
           />
         ))}
       </div>
