20668de0edc00d09affb11046a9d9651a15e126e
maojude <maojude04@gmail.com>
2025-05-12 18:34:26 +0800

    27/04/2025 - Created Expense List Component

---
+++ frontend/expense-tracker/src/components/Expense/ExpenseList.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import moment from "moment";
+import TransactionInfoCard from "../Cards/TransactionInfoCard";
+
+const ExpenseList = ({ transactions, onDelete }) => {
+  return (
+    <div className="card">
+      <div className="flex items-center justify-between">
+        <h5 className="text-lg">Expense List</h5>
+      </div>
+
+      <div className="grid grid-cols-1 mid:grid-cols-2">
+        {transactions?.map((expense) => (
+          <TransactionInfoCard
+            key={expense._id}
+            title={expense.category}
+            icon={expense.icon}
+            date={moment(expense.date).format("Do MMM YYYY")} //moment converts raw date to a readable format
+            amount={expense.amount}
+            type="expense"
+            onDelete={() => onDelete(expense._id)}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ExpenseList;
